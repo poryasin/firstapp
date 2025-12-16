aadffb39b9b6aeb5d1e363217c91f7ccd7a2e9ba
poryasin <poraya_sin@cmu.ac.th>
2025-12-16 09:51:01 +0700

    Add pages

---
+++ lib/pages/cal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculatePage extends StatefulWidget {
+  const CalculatePage({super.key});
+
+  @override
+  State<CalculatePage> createState() => _CalculatePageStage();
+}
+
+class _CalculatePageStage extends State<CalculatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
